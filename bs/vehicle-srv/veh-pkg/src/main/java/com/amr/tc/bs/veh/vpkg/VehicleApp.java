e851348688418b346ae71882876cd24fcdc0a7eb
andresromerol7 <112513294+andresmromero@users.noreply.github.com>
2023-05-30 18:43:07 -0500

    Add modular structure

---
+++ bs/vehicle-srv/veh-pkg/src/main/java/com/amr/tc/bs/veh/vpkg/VehicleApp.java
@@ -0,0 +1,15 @@
+package com.amr.tc.bs.veh.vpkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(VehicleApp.class, args);
+
+    }
+
+}
